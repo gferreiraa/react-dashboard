c0ac34599068d3299823d5a3f024a793fdd31993
gferreira <gferreiraa@gmail.com>
2020-08-17 02:00:29 -0300

    FEATURE | Aside, react-icons, styles inclued

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import { Container } from './styles'
+import { Container, Header, Brand, MenuContainer, MenuItemLink, Title } from './styles';
+import brandImg from '../../assets/logo.svg';
+import { AiFillDashboard, AiOutlineUpCircle, AiOutlineDownCircle, AiOutlineLogout } from 'react-icons/ai';
+
+
 const Aside: React.FC = () => {
   return(
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <Brand src={brandImg}  alt= 'Company Brand'/>
+        <Title>My Wallet</Title>
+      </Header>
+      <MenuContainer>
+        <MenuItemLink href='#'>
+          <AiFillDashboard/>
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href='#'>
+          <AiOutlineUpCircle/>
+          Down Payments
+        </MenuItemLink>
+        <MenuItemLink href='#'>
+          <AiOutlineDownCircle/>
+          Expenses
+        </MenuItemLink>
+        <MenuItemLink href='#'>
+          <AiOutlineLogout/>
+          Logout
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
