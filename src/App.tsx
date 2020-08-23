1d8b39236e8a63676b023eb4941a5998d6c26743
gferreira <gferreiraa@gmail.com>
2020-08-23 01:57:49 -0300

    FEATURE | Dynamic header content, select and styles inclued

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { ThemeProvider } from 'styled-components';
 import Layout from './components/Layout';
 import dark from './styles/themes/dark';
 import Dashboard from './pages/Dashboard';
+import List from './pages/List';
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyles/>
       <Layout>
-        <Dashboard/>
+        <List/>
       </Layout>
     </ThemeProvider>
   );
